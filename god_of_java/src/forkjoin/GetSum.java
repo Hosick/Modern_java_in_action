1d9d7f1f42fc0557173d7b35addd27d9b137fafe
Hosick <ghtjr5000@naver.com>
2020-11-27 18:43:35 +0900

    feat: forkjoin example

---
+++ god_of_java/src/forkjoin/GetSum.java
@@ -0,0 +1,31 @@
+package forkjoin;
+
+import java.util.concurrent.RecursiveTask;
+
+public class GetSum extends RecursiveTask<Long> {
+    long from;
+    long to;
+
+    public GetSum(long from, long to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public Long compute() {
+        long gap = to - from;
+
+        if (gap <= 3) {
+            long tempSum = 0;
+            for (long loop = from; loop <= to; loop++) {
+                tempSum += loop;
+            }
+            return tempSum;
+        }
+
+        long middle = (from + to) / 2;
+        GetSum sumpre = new GetSum(from, middle);
+        sumpre.fork();
+        GetSum sumPost = new GetSum(middle + 1, to);
+        return sumPost.compute() + sumpre.join();
+    }
+}
